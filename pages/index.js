ca7f38cc064164595712a91951d260e8b9c2ea9c
yuragms <yura.gms@gmail.com>
2023-06-02 17:39:13 +0300

    3-scss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 // import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/Home.module.scss';
 
 // const inter = Inter({ subsets: ['latin'] })
 // <main className={`${styles.main} ${inter.className}`}></main>
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome</h1>
+      <h1 className={styles.red}>Welcome</h1>
     </>
   );
 }
