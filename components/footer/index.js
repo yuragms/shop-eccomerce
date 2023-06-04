4df960f6fbe2f0a47158e424a0b8f7101df8b15b
yuragms <yura.gms@gmail.com>
2023-06-04 23:25:28 +0300

    16-footer-paymment-copyriht

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,6 @@
 import Links from './Links';
+import NewsLetter from './NewsLetter';
+import Socials from './Socials';
 import styles from './styles.module.scss';
 
 export default function Footer() {
@@ -6,6 +8,8 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footer__container}>
         <Links />
+        <Socials />
+        <NewsLetter />
       </div>
     </footer>
   );
