c55520b4db35eff95f484791807140f54cf92f1d
yuragms <yura.gms@gmail.com>
2023-06-05 10:10:30 +0300

    16-footer-payment-copyright2

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,5 +1,7 @@
+import Copyright from './Copyright';
 import Links from './Links';
 import NewsLetter from './NewsLetter';
+import Payment from './Payment';
 import Socials from './Socials';
 import styles from './styles.module.scss';
 
@@ -10,6 +12,8 @@ export default function Footer() {
         <Links />
         <Socials />
         <NewsLetter />
+        <Payment />
+        <Copyright />
       </div>
     </footer>
   );
