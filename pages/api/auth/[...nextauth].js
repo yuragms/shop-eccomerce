39710278dc892c44779c463c42183acfa9626a38
yuragms <yura.gms@gmail.com>
2023-06-11 12:18:40 +0300

    31-signin-page6

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import Auth0Provider from 'next-auth/providers/auth0';
+// import Auth0Provider from 'next-auth/providers/auth0';
 import clientPromise from './lib/mongodb';
 
 export default NextAuth({
@@ -32,11 +32,11 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
+    // Auth0Provider({
+    //   clientId: process.env.AUTH0_CLIENT_ID,
+    //   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    //   issuer: process.env.AUTH0_ISSUER,
+    // }),
   ],
   pages: {
     signIn: '/signin',
