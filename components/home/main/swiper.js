04c18145d9c6f8111b4296f87d4e0ec7c41ed67c
yuragms <yura.gms@gmail.com>
2023-06-14 18:46:19 +0300

    48-home-swiper1

--- components/home/main/swiper.js
+++ components/home/main/swiper.js
@@ -24,17 +24,11 @@ export default function MainSwiper() {
         modules={[Pagination, Navigation]}
         className="mainSwiper"
       >
-        <SwiperSlide>
-          <img src="../../../images/swiper/1.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {[...Array(10).keys()].map((i) => (
+          <SwiperSlide>
+            <img src={`../../../images/swiper/${i + 1}.jpg`} alt="" />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
