2a5cf0aaf57604d5800681da4eddfa1962d2fc67
yuragms <yura.gms@gmail.com>
2023-06-14 22:23:19 +0300

    50-home-offers-main

---
+++ components/home/main/offers.js
@@ -0,0 +1,41 @@
+import styles from './styles.module.scss';
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination } from 'swiper';
+import { offersAarray } from '@/data/home';
+import Link from 'next/link';
+
+export default function Offers() {
+  return (
+    <div className={styles.offers}>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="offers_swiper"
+      >
+        {offersAarray.map((offer) => (
+          <SwiperSlide>
+            <Link href="">
+              <img src={offer.image} alt="" />
+            </Link>
+            <span>{offer.price}$</span>
+            <span>-{offer.discount}%</span>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
