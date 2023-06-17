62ef529e4e2d3da6fea4738eedfe21f0613b8830
yuragms <yura.gms@gmail.com>
2023-06-17 22:11:23 +0300

    58-flash-deals-countdown

--- components/home/flashDeals/index.js
+++ components/home/flashDeals/index.js
@@ -26,11 +26,28 @@ export default function FlashDeals() {
         <Countdown />
       </div>
       <Swiper
-        slidesPerView={6}
-        spaceBetween={20}
+        slidesPerView={1}
+        spaceBetween={10}
         navigation={true}
         modules={[Navigation]}
         className="flashDeals__swiper"
+        breakpoints={{
+          450: {
+            slidesPerView: 2,
+          },
+          630: {
+            slidesPerView: 3,
+          },
+          920: {
+            slidesPerView: 4,
+          },
+          1232: {
+            slidesPerView: 5,
+          },
+          1520: {
+            slidesPerView: 6,
+          },
+        }}
       >
         <div className={styles.flashDeals__list}>
           {flashDealsArray.map((product, i) => (
