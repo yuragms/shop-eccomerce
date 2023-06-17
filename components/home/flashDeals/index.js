527bbe3bb06bf7184d50bbbc16066e72a6df16a6
yuragms <yura.gms@gmail.com>
2023-06-17 14:41:37 +0300

    57-flash-deals2

--- components/home/flashDeals/index.js
+++ components/home/flashDeals/index.js
@@ -26,8 +26,8 @@ export default function FlashDeals() {
         <Countdown />
       </div>
       <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
+        slidesPerView={6}
+        spaceBetween={20}
         navigation={true}
         modules={[Navigation]}
         className="flashDeals__swiper"
