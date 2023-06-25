46713f3bb0dc8071a096acc00dcb0217df1564bb
yuragms <yura.gms@gmail.com>
2023-06-25 11:24:13 +0300

    64-product-model

---
+++ models/Product.js
@@ -0,0 +1,130 @@
+import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+const reviewSchema = new mongoose.Schema({
+  reviewBy: {
+    type: ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  review: {
+    type: Number,
+    required: true,
+  },
+  size: {
+    type: Number,
+  },
+  style: {
+    color: String,
+    image: String,
+  },
+  fit: {
+    type: String,
+  },
+  imfges: [],
+  likes: [],
+});
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      required: true,
+      ref: 'Category',
+    },
+    subCategories: [
+      {
+        type: ObjectId,
+        ref: 'subCategories',
+      },
+    ],
+    details: [
+      {
+        name: String,
+        value: String,
+      },
+    ],
+    questions: [
+      {
+        question: String,
+        answer: String,
+      },
+    ],
+    reviews: [reviewSchema],
+    refundPolicy: {
+      type: String,
+      default: '30 days',
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shipping: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    subProducts: [
+      {
+        images: [],
+        description_images: [],
+        color: {
+          color: {
+            type: String,
+          },
+          image: {
+            type: String,
+          },
+        },
+        sizes: [
+          {
+            size: String,
+            qty: Number,
+            price: Number,
+          },
+        ],
+        discount: {
+          type: Number,
+          default: 0,
+        },
+        sold: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product =
+  mongoose.models.Product || mongoose.model('Product', productSchema);
+export default Product;
