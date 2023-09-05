628be7b92cc297a1aa9ebc62e37afa32b82cf506
yuragms <yura.gms@gmail.com>
2023-09-05 20:47:08 +0300

    70-get-filter-prepare-data

--- models/Product.js
+++ models/Product.js
@@ -95,6 +95,7 @@ const productSchema = new mongoose.Schema(
     },
     subProducts: [
       {
+        sku: String,
         images: [],
         description_images: [],
         color: {
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,7 +2,8 @@ import db from '@/utils/db';
 import styles from '../../styles/product.module.scss';
 import Product from '@/models/Product';
 
-export default function product() {
+export default function product({ product }) {
+  console.log(product);
   return <div>[SLUG]</div>;
 }
 
@@ -41,7 +42,7 @@ export async function getServerSideProps(context) {
     .sort((a, b) => {
       return a - b;
     });
-  console.log(prices);
+  console.log(subProduct);
 
   let newProduct = {
     ...product,
@@ -52,13 +53,28 @@ export async function getServerSideProps(context) {
     colors: product.subProducts.map((p) => {
       return p.color;
     }),
+    priceRange:
+      prices.length > 1
+        ? `From ${prices[0]} to ${prices[prices.length - 1]}$`
+        : '',
+    price:
+      subProduct.discount > 0
+        ? (
+            subProduct.sizes[size].price -
+            subProduct.sizes[size].price / subProduct.discount
+          ).toFixed(2)
+        : subProduct.sizes[size].price,
+    priceBefore: subProduct.sizes[size].price,
+    quantity: subProduct.sizes[size].qty,
   };
 
-  console.log(prices);
   //--------
+  console.log('new product', newProduct);
   db.disconnectDb();
 
   return {
-    props: {},
+    props: {
+      product: JSON.parse(JSON.stringify(newProduct)),
+    },
   };
 }
