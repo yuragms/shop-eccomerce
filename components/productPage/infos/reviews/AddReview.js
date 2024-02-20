f5047b6e4ade78898451ee100f4d20e63f42b995
yuragms <yura.gms@gmail.com>
2024-02-20 10:43:03 +0200

    81-2-34

--- components/productPage/infos/reviews/AddReview.js
+++ components/productPage/infos/reviews/AddReview.js
@@ -4,12 +4,20 @@ import styles from './styles.module.scss';
 
 export default function AddReview({ product }) {
   const [size, setSize] = useState('second');
+  const handleSize = (size) => {
+    setSize(size);
+  };
   return (
     <div className={styles.reviews__add}>
       <div className="flex wrap">
         <div className="flex" style={{ gap: '10px' }}>
           Size:
-          <Select property={size} text="Size" data={product.allSizes} />
+          <Select
+            property={size}
+            text="Size"
+            data={product.allSizes}
+            handleChange={handleSize}
+          />
         </div>
       </div>
     </div>
--- components/productPage/infos/reviews/Select.js
+++ components/productPage/infos/reviews/Select.js
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import styles from './styles.module.scss';
 import { IoArrowDown } from 'react-icons/io5';
 
-export default function Select({ property, text, data }) {
+export default function Select({ property, text, data, handleChange }) {
   const [visible, setVisible] = useState(true);
   return (
     <div className={styles.select}>
-      <div className={styles.select__header}>
+      <div
+        className={styles.select__header}
+        onMouseOver={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+      >
         <span
           className={`flex ${styles.select__header_wrap}`}
           style={{ padding: '0 5px' }}
@@ -15,11 +19,15 @@ export default function Select({ property, text, data }) {
           <IoArrowDown />
         </span>
         {visible && (
-          <ul className={styles.select__header_menu}>
+          <ul
+            className={styles.select__header_menu}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
             {data.map((item, i) => {
               if (text == 'Size') {
                 return (
-                  <li key={i}>
+                  <li key={i} onClick={() => handleChange(item.size)}>
                     <span>{item.size}</span>
                   </li>
                 );
