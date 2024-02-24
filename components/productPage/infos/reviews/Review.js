1c42b85e4b9ccfb8ee32b540165c3c8905569751
yuragms <yura.gms@gmail.com>
2024-02-24 16:50:50 +0200

    85-review card

--- components/productPage/infos/reviews/Review.js
+++ components/productPage/infos/reviews/Review.js
@@ -1,5 +1,6 @@
 import { Rating } from '@mui/material';
 import styles from './styles.module.scss';
+import { AiOutlineLike } from 'react-icons/ai';
 
 export default function Review({ review }) {
   console.log(review);
@@ -13,12 +14,44 @@ export default function Review({ review }) {
           </h4>
           <img src={image} alt="" />
         </div>
-        <div className={styles.review}>
+        <div className={styles.review__review}>
           <Rating
             name="half-rating-read"
             defaultValue={review.rating}
-            readOnlystyle={{ color: '#facf19' }}
+            readOnly
+            style={{ color: '#facf19' }}
           />
+          <p>{review.review}</p>
+          <p>
+            <span>Overall Fit:</span>
+            {review.fit}
+            &nbsp;&nbsp;
+            <span>Size:</span>
+            {review.size}
+            &nbsp;&nbsp;
+            <div className={styles.flex}>
+              <img
+                src={review.style.image}
+                alt=""
+                className={styles.review__img}
+              />
+            </div>
+          </p>
+        </div>
+      </div>
+      <div className={styles.flex}>
+        <div className={styles.review__images}>
+          {review.images.length > 0 &&
+            review.images.map((img) => <img src={img?.url} alt="" />)}
+        </div>
+        <div className={styles.review__extra}>
+          <div className={styles.review__extra_likes}>
+            {review.likes && review.likes?.likes}
+            <AiOutlineLike />
+          </div>
+          <div className={styles.review__extra_date}>
+            {review.updatedAt.slice(0, 10)}
+          </div>
         </div>
       </div>
     </div>
