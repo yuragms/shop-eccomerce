dab09643070cf84ed532470950d6ba6983c6ab19
yuragms <yura.gms@gmail.com>
2024-03-06 23:25:43 +0200

    91-cart-product-card-styles

--- components/cart/product/index.js
+++ components/cart/product/index.js
@@ -2,8 +2,10 @@ import { BsHeart } from 'react-icons/bs';
 import styles from './styles.module.scss';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { useState } from 'react';
 
 export default function Product({ product }) {
+  const [active, setActive] = useState();
   return (
     <div className={`${styles.card} ${styles.product}`}>
       {product.quantity < 1 && <div className={styles.blur}></div>}
@@ -12,6 +14,13 @@ export default function Product({ product }) {
         M74JJI Official Store
       </div>
       <div className={styles.product__image}>
+        <div
+          className={`
+          ${styles.checkbox} 
+          ${active ? styles.active : ''}
+          `}
+          onClick={() => handleSelect()}
+        ></div>
         <img src={product.images[0].url} alt="" />
         <div className={styles.col}>
           <div className={styles.grid}>
@@ -30,7 +39,7 @@ export default function Product({ product }) {
           <div className={styles.product__style}>
             <img src={product.color.image} alt="" />
             {product.size && <span>{product.size}</span>}
-            {product.price && <span>{product.price.toFixed(2)}</span>}
+            {product.price && <span>{product.price.toFixed(2)}$</span>}
             <MdOutlineKeyboardArrowRight />
           </div>
           <div className={styles.product__priceQty}>
@@ -52,18 +61,18 @@ export default function Product({ product }) {
               <span>{product.qty}</span>
               <button disabled={product.qty == product.quantity}>+</button>
             </div>
-            <div className={styles.product__priceQty_shipping}>
-              {product.shipping
-                ? `+${product.shipping} Shipping fee`
-                : 'Free Shipping'}
-            </div>
-            {product.quantity < 1 && (
-              <div className={styles.notAvailable}>
-                This product is out of stock, Add it to ypur whishlist it may
-                get restocked.
-              </div>
-            )}
           </div>
+          <div className={styles.product__shipping}>
+            {product.shipping
+              ? `+${product.shipping} Shipping fee`
+              : 'Free Shipping'}
+          </div>
+          {product.quantity < 1 && (
+            <div className={styles.notAvailable}>
+              This product is out of stock, Add it to ypur whishlist it may get
+              restocked.
+            </div>
+          )}
         </div>
       </div>
     </div>
