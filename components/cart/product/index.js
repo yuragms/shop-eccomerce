adbd730a128a8d1dfce54c2824f728996f4e0ddd
yuragms <yura.gms@gmail.com>
2024-03-06 13:04:34 +0200

    90-cart-product-card-markup

---
+++ components/cart/product/index.js
@@ -0,0 +1,71 @@
+import { BsHeart } from 'react-icons/bs';
+import styles from './styles.module.scss';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+export default function Product({ product }) {
+  return (
+    <div className={`${styles.card} ${styles.product}`}>
+      {product.quantity < 1 && <div className={styles.blur}></div>}
+      <div className={styles.product__header}>
+        <img src="../../../images/store.webp" alt="" />
+        M74JJI Official Store
+      </div>
+      <div className={styles.product__image}>
+        <img src={product.images[0].url} alt="" />
+        <div className={styles.col}>
+          <div className={styles.grid}>
+            <h1>
+              {product.name.length > 30
+                ? `${product.name.substring(0, 30)}`
+                : product.name}
+            </h1>
+            <div style={{ zIndex: '2' }}>
+              <BsHeart />
+            </div>
+            <div style={{ zIndex: '2' }}>
+              <AiOutlineDelete />
+            </div>
+          </div>
+          <div className={styles.product__style}>
+            <img src={product.color.image} alt="" />
+            {product.size && <span>{product.size}</span>}
+            {product.price && <span>{product.price.toFixed(2)}</span>}
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className={styles.product__priceQty}>
+            <div className={styles.product__priceQty_price}>
+              <span className={styles.price}>
+                USD{(product.price * product.qty).toFixed(2)}$
+              </span>
+              {product.price !== product.priceBefore && (
+                <span className={styles.priceBefore}>
+                  USD{product.priceBefore}$
+                </span>
+              )}
+              {product.discount > 0 && (
+                <span className={styles.discount}>-{product.discount}%</span>
+              )}
+            </div>
+            <div className={styles.product__priceQty_qty}>
+              <button disabled={product.qty < 2}>-</button>
+              <span>{product.qty}</span>
+              <button disabled={product.qty == product.quantity}>+</button>
+            </div>
+            <div className={styles.product__priceQty_shipping}>
+              {product.shipping
+                ? `+${product.shipping} Shipping fee`
+                : 'Free Shipping'}
+            </div>
+            {product.quantity < 1 && (
+              <div className={styles.notAvailable}>
+                This product is out of stock, Add it to ypur whishlist it may
+                get restocked.
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/cart.js
+++ pages/cart.js
@@ -2,15 +2,23 @@ import Header from '@/components/cart/header';
 import styles from '../styles/cart.module.scss';
 import React from 'react';
 import Empty from '@/components/cart/empty';
+import { useSelector } from 'react-redux';
+import Product from '@/components/cart/product';
 
 export default function cart() {
-  const cart = [];
+  const { cart } = useSelector((state) => ({ ...state }));
   return (
     <>
       <Header />
       <div className={styles.cart}>
-        {cart.length > 1 ? (
-          <div className={styles.cart__container}></div>
+        {cart.cartItems.length > 1 ? (
+          <div className={styles.cart__container}>
+            <div className={styles.cart__products}>
+              {cart.cartItems.map((product) => (
+                <Product product={product} key={product._uid} />
+              ))}
+            </div>
+          </div>
         ) : (
           <Empty />
         )}
