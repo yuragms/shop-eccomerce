83030a5113411218403aeb4ed698b863397e13db
yuragms <yura.gms@gmail.com>
2024-03-10 11:12:22 +0200

    1

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -1,7 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+  cartItems: [],
+};
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      state.cartItems.push(action.payload);
+    },
+    updateCart(state, action) {
+      state.cartItems = action.payload;
+    },
+    emptyCart(state) {
+      state.cartItems = [];
+      // return { ...state.cart, cartItems: [] };
+    },
+  },
+});
+
+export const { addToCart, updateCart, emptyCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
+
 // import { createSlice } from '@reduxjs/toolkit';
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
 // const initialState = {
 //   cartItems: [],
 // };
+
 // export const cartSlice = createSlice({
 //   name: 'cart',
 //   initialState,
@@ -20,39 +54,26 @@
 
 // export const { addToCart, updateCart, emptyCart } = cartSlice.actions;
 
-// export default cartSlice.reducer;
+// const persistedReducer = persistReducer(persistConfig, cartSlice.reducer);
 
-import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// export default persistedReducer;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
+// import { createSlice, nanoid } from '@reduxjs/toolkit';
 
-const initialState = {
-  cartItems: [],
-};
+// const todosSlice = createSlice({
+//   name: 'todos',
+//   initialState: [],
+//   reducers: {
+//     addTodo: {
+//       reducer: (state, action) => {
+//         state.push(action.payload);
+//       },
+//       prepare: (text) => {
+//         const id = nanoid();
+//         return { payload: { id, text } };
+//       },
+//     },
+//   },
+// });
 
-export const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-    addToCart(state, action) {
-      state.cartItems.push(action.payload);
-    },
-    updateCart(state, action) {
-      state.cartItems = action.payload;
-    },
-    emptyCart(state, action) {
-      state.cartItems = [];
-    },
-  },
-});
-
-export const { addToCart, updateCart, emptyCart } = cartSlice.actions;
-
-const persistedReducer = persistReducer(persistConfig, cartSlice.reducer);
-
-export default persistedReducer;
+// https://redux-toolkit.js.org/api/createslice
