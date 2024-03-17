91c004a74d9bbb57b8afb442aff19ce72abee42f
yuragms <yura.gms@gmail.com>
2024-03-17 12:19:19 +0200

    101-4-54

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,5 +1,20 @@
+import { getSession } from 'next-auth/react';
 import styles from '../styles/checkout.module.scss';
+import { Context } from 'react-responsive';
+import User from '@/models/User';
+import Cart from '@/models/Cart';
 
 export default function checkout() {
   return <div>checkout</div>;
+}
+
+export async function getServerSideProps(Context) {
+  // console.log(Context);
+  const session = await getSession(Context);
+  const user = await User.findById(session.user.id);
+  const cart = await Cart.findOne({ user: user._id });
+  console.log(cart);
+  return {
+    props: {},
+  };
 }
