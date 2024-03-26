3ad01b9fa9f7b6a99dac5f77168e99dff4a9c424
yuragms <yura.gms@gmail.com>
2024-03-26 12:40:48 +0200

    108-list-all-addresses-and-change-active-status2

--- components/checkout/shipping/index.js
+++ components/checkout/shipping/index.js
@@ -87,6 +87,7 @@ export default function Shipping({
     setShipping({ ...shipping, [name]: value });
   };
   const saveShippingHandler = async () => {
+    console.log('saveShippingHandler');
     const res = await saveAddress(shipping);
     console.log(res);
     setAddresses(res.addresses);
@@ -113,8 +114,8 @@ export default function Shipping({
             <div className={styles.address__col}>
               <span>
                 <FaIdCard />
-                console.log(address)
-                {address.firstName.toUpperCase()}{' '}
+                {console.log(address)}
+                {/* {address.firstName.toUpperCase()}{' '} */}
                 {address.lastName.toUpperCase()}
               </span>
               <span>
@@ -177,24 +178,6 @@ export default function Shipping({
         >
           {(formik) => (
             <Form>
-              {/* <FormControl className={styles.select}>
-              <InputLabel id="demo-simple-select-helper-label">
-                Country
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                value={country}
-                name="country"
-                onChange={handleChange}
-              >
-                {countries.map((country) => (
-                  <MenuItem value={country.name} key={country.name}>
-                    {country.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl> */}
               <SingularSelect
                 name="country"
                 value={country}
@@ -254,3 +237,229 @@ export default function Shipping({
     </div>
   );
 }
+
+// const initialValues = {
+//   firstName: '',
+//   lastName: '',
+//   phoneNumber: '',
+//   state: '',
+//   city: '',
+//   zipCode: '',
+//   address1: '',
+//   address2: '',
+//   country: '',
+// };
+// export default function Shipping({ user, addresses, setAddresses, profile }) {
+//   const [shipping, setShipping] = useState(initialValues);
+//   const [visible, setVisible] = useState(user?.address.length ? false : true);
+//   const {
+//     firstName,
+//     lastName,
+//     phoneNumber,
+//     state,
+//     city,
+//     zipCode,
+//     address1,
+//     address2,
+//     country,
+//   } = shipping;
+//   const validate = Yup.object({
+//     firstName: Yup.string()
+//       .required('First name is required.')
+//       .min(3, 'First name must be atleast 3 characters long.')
+//       .max(20, 'First name must be less than 20 characters long.'),
+//     lastName: Yup.string()
+//       .required('Last name is required.')
+//       .min(3, 'Last name must be atleast 3 characters long.')
+//       .max(20, 'Last name must be less than 20 characters long.'),
+//     phoneNumber: Yup.string()
+//       .required('Phone number is required.')
+//       .phone()
+//       .min(3, 'Phone number must be atleast 3 characters long.')
+//       .max(30, 'Phone number must be less than 20 characters long.'),
+//     state: Yup.string()
+//       .required('State name is required.')
+//       .min(2, 'State name should contain 2-60 characters..')
+//       .max(60, 'State name should contain 2-60 characters.'),
+//     city: Yup.string()
+//       .required('City name is required.')
+//       .min(2, 'City name should contain 2-60 characters.')
+//       .max(60, 'City name should contain 2-60 characters.'),
+//     zipCode: Yup.string()
+//       .required('ZipCode/Postal is required.')
+//       .min(2, 'ZipCode/Postal should contain 2-30 characters..')
+//       .max(30, 'ZipCode/Postal should contain 2-30 characters.'),
+//     address1: Yup.string()
+//       .required('Address Line 1 is required.')
+//       .min(5, 'Address Line 1 should contain 5-100 characters.')
+//       .max(100, 'Address Line 1 should contain 5-100 characters.'),
+//     address2: Yup.string()
+//       .min(5, 'Address Line 2 should contain 5-100 characters.')
+//       .max(100, 'Address Line 2 should contain 5-100 characters.'),
+//     country: Yup.string().required('Country name is required.'),
+//   });
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setShipping({ ...shipping, [name]: value });
+//   };
+//   const saveShippingHandler = async () => {
+//     const res = await saveAddress(shipping);
+//     setAddresses(res.addresses);
+//   };
+//   const changeActiveHandler = async (id) => {
+//     const res = await changeActiveAddress(id);
+//     setAddresses(res.addresses);
+//   };
+//   const deleteHandler = async (id) => {
+//     const res = await deleteAddress(id);
+//     setAddresses(res.addresses);
+//   };
+//   return (
+//     <div className={styles.shipping}>
+//       {!profile && (
+//         <div className={styles.header}>
+//           <h3>Shipping Informations</h3>
+//         </div>
+//       )}
+//       <div className={styles.addresses}>
+//         {addresses.map((address) => (
+//           <div style={{ position: 'relative' }}>
+//             <div
+//               className={styles.address__delete}
+//               onClick={() => deleteHandler(address._id)}
+//             >
+//               {/* <IoIosRemoveCircleOutline /> */}
+//             </div>
+//             <div
+//               className={`${styles.address} ${address.active && styles.active}`}
+//               key={address._id}
+//               onClick={() => changeActiveHandler(address._id)}
+//             >
+//               <div className={styles.address__side}>
+//                 <img src={profile ? user.user.image : user.image} alt="" />
+//               </div>
+//               <div className={styles.address__col}>
+//                 <span>
+//                   <FaIdCard />
+//                   {address.firstName.toUpperCase()}{' '}
+//                   {address.lastName.toUpperCase()}
+//                 </span>
+//                 <span>
+//                   <GiPhone />
+//                   {address.phoneNumber}
+//                 </span>
+//               </div>
+//               <div className={styles.address__col}>
+//                 <span>
+//                   <FaMapMarkerAlt />
+//                   {address.address1}
+//                 </span>
+//                 <span>{address.address2}</span>
+//                 <span>
+//                   {address.city},{address.state},{address.country}
+//                 </span>
+//                 <span>{address.zipCode}</span>
+//               </div>
+//               <span
+//                 className={styles.active__text}
+//                 style={{
+//                   display: `${!address.active && 'none'}`,
+//                 }}
+//               >
+//                 Active
+//               </span>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//       <button className={styles.hide_show} onClick={() => setVisible(!visible)}>
+//         {visible ? (
+//           <span>
+//             <IoMdArrowDropupCircle style={{ fontSize: '2rem', fill: '#222' }} />
+//           </span>
+//         ) : (
+//           <span>
+//             ADD NEW ADDRESS <AiOutlinePlus />
+//           </span>
+//         )}
+//       </button>
+//       {visible && (
+//         <Formik
+//           enableReinitialize
+//           initialValues={{
+//             firstName,
+//             lastName,
+//             phoneNumber,
+//             state,
+//             city,
+//             zipCode,
+//             address1,
+//             address2,
+//             country,
+//           }}
+//           validationSchema={validate}
+//           onSubmit={() => {
+//             saveShippingHandler();
+//           }}
+//         >
+//           {(formik) => (
+//             <Form>
+//               <SingularSelect
+//                 name="country"
+//                 value={country}
+//                 placeholder="*Country"
+//                 handleChange={handleChange}
+//                 data={countries}
+//               />
+//               <div className={styles.col}>
+//                 <ShippingInput
+//                   name="firstName"
+//                   placeholder="*First Name"
+//                   onChange={handleChange}
+//                 />
+//                 <ShippingInput
+//                   name="lastName"
+//                   placeholder="*Last Name"
+//                   onChange={handleChange}
+//                 />
+//               </div>
+//               <div className={styles.col}>
+//                 <ShippingInput
+//                   name="state"
+//                   placeholder="*State/Province"
+//                   onChange={handleChange}
+//                 />
+//                 <ShippingInput
+//                   name="city"
+//                   placeholder="*City"
+//                   onChange={handleChange}
+//                 />
+//               </div>
+//               <ShippingInput
+//                 name="phoneNumber"
+//                 placeholder="*Phone number"
+//                 onChange={handleChange}
+//               />
+//               <ShippingInput
+//                 name="zipCode"
+//                 placeholder="*Post/Zip code"
+//                 onChange={handleChange}
+//               />
+//               <ShippingInput
+//                 name="address1"
+//                 placeholder="Address 1"
+//                 onChange={handleChange}
+//               />
+//               <ShippingInput
+//                 name="address2"
+//                 placeholder="Address 2"
+//                 onChange={handleChange}
+//               />
+//               <button type="submit">Save Address</button>
+//             </Form>
+//           )}
+//         </Formik>
+//       )}
+//     </div>
+//   );
+// }
--- middware/auth.js
+++ middware/auth.js
@@ -1,35 +1,37 @@
-// import { getToken } from 'next-auth/jwt';
-
-// export default async (req, res, next) => {
-//   const token = await getToken({
-//     req,
-//     secret: process.env.JWT_SECRET,
-//     secureCookie: process.env.NODE_ENV === 'production',
-//   });
-//   if (token) {
-//     //signed
-//     req.user = token.sub;
-//     next();
-//   } else {
-//     res.status(401).json({ message: 'Not signed in:' });
-//   }
-//   res.end();
-// };
-
-// выдает ошибку в 3000/checkout при попытке добавления адреса
 import { getToken } from 'next-auth/jwt';
 
-export default async (req, res) => {
+export default async (req, res, next) => {
   const token = await getToken({
     req,
     secret: process.env.JWT_SECRET,
     secureCookie: process.env.NODE_ENV === 'production',
   });
-
   if (token) {
+    //signed in
     req.user = token.sub;
+    next();
   } else {
-    res.status(401).json({ message: 'Not signed in:' });
-    res.end();
+    res.status(401).json({ message: 'Not signed in :' });
   }
+  /*
+  res.end();
+  */
 };
+
+// выдает ошибку в 3000/checkout при попытке добавления адреса
+// import { getToken } from 'next-auth/jwt';
+
+// export default async (req, res) => {
+//   const token = await getToken({
+//     req,
+//     secret: process.env.JWT_SECRET,
+//     secureCookie: process.env.NODE_ENV === 'production',
+//   });
+
+//   if (token) {
+//     req.user = token.sub;
+//   } else {
+//     res.status(401).json({ message: 'Not signed in:' });
+//     res.end();
+//   }
+// };
