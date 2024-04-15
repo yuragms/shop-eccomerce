96736e82b19635ee77d1928aa33ab28fe164babc
yuragms <yura.gms@gmail.com>
2024-04-15 20:02:37 +0300

    132-admin-dashboard-sidebar-part3

--- components/admin/layout/sidebar/index.js
+++ components/admin/layout/sidebar/index.js
@@ -1,11 +1,27 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles.module.scss';
 import { toggleSidebar } from '@/store/ExpandSlice';
-import { MdArrowForwardIos, MdSpaceDashboard } from 'react-icons/md';
+//---------------------------------------------------------------
+import {
+  MdArrowForwardIos,
+  MdOutlineCategory,
+  MdSpaceDashboard,
+} from 'react-icons/md';
+import { FcSalesPerformance } from 'react-icons/fc';
+import { IoListCircleSharp } from 'react-icons/io5';
+import { ImUsers } from 'react-icons/im';
+import { AiFillMessage } from 'react-icons/ai';
+import { FaThList } from 'react-icons/fa';
+import { BsPatchPlus } from 'react-icons/bs';
+import { RiCoupon3Fill } from 'react-icons/ri';
+//---------------------------------------------------------------
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Sidebar() {
+  const router = useRouter();
+  const route = router.pathname.split('/admin/dashboard')[1];
   const { data: session } = useSession({ required: true });
 
   const dispatch = useDispatch();
@@ -14,7 +30,7 @@ export default function Sidebar() {
   const handleExpand = () => {
     dispatch(toggleSidebar());
   };
-  console.log(useSession);
+
   return (
     <div className={`${styles.sidebar} ${expand ? styles.opened : ''}`}>
       <div className={styles.sidebar__toggle} onClick={() => handleExpand()}>
@@ -37,17 +53,96 @@ export default function Sidebar() {
           <img src={session?.user?.image} alt="" />
           <div className={styles.show}>
             <span>Welcome back 👋</span>
-            <span>{session?.user?.image}</span>
+            <span>{session?.user?.name}</span>
           </div>
         </div>
-        <ul>
-          <li>
-            <Link href="">
+        <ul className={styles.sidebar__list}>
+          <li className={route == undefined ? styles.active : ''}>
+            <Link href="/admin/dashboard">
               <MdSpaceDashboard />
               <span className={styles.show}>Dashboard</span>
             </Link>
           </li>
+          <li className={route == 'sales' ? styles.active : ''}>
+            <Link href="/admin/dashboard/sales">
+              <FcSalesPerformance />
+              <span className={styles.show}>Sales</span>
+            </Link>
+          </li>
+          <li className={route == 'orders' ? styles.active : ''}>
+            <Link href="/admin/dashboard/orders">
+              <IoListCircleSharp />
+              <span className={styles.show}>Orders</span>
+            </Link>
+          </li>
+          <li className={route == 'users' ? styles.active : ''}>
+            <Link href="/admin/dashboard/users">
+              <ImUsers />
+              <span className={styles.show}>Users</span>
+            </Link>
+          </li>
+          <li className={route == 'messages' ? styles.active : ''}>
+            <Link href="/admin/dashboard">
+              <AiFillMessage />
+              <span className={styles.show}>Messages</span>
+            </Link>
+          </li>
         </ul>
+        <div className={styles.sidebar__dropdown}>
+          <div className={styles.sidebar__dropdown_heading}>
+            <div className={styles.show}>Product</div>
+          </div>
+          <ul className={styles.sidebar__list}>
+            <li className={route == 'product/all' ? styles.active : ''}>
+              <Link href="/admin/dashboard/product/all">
+                <FaThList />
+                <span className={styles.show}>All products</span>
+              </Link>
+            </li>
+            <li className={route == 'product/create' ? styles.active : ''}>
+              <Link href="/admin/dashboard/product/create">
+                <BsPatchPlus />
+                <span className={styles.show}>Create Products</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.sidebar__dropdown}>
+          <div className={styles.sidebar__dropdown_heading}>
+            <div className={styles.show}>Categories / Subs</div>
+          </div>
+          <ul className={styles.sidebar__list}>
+            <li className={route == 'categories' ? styles.active : ''}>
+              <Link href="/admin/dashboard/categories">
+                <MdOutlineCategory />
+                <span className={styles.show}>Categories</span>
+              </Link>
+            </li>
+            <li className={route == 'subCategories' ? styles.active : ''}>
+              <Link href="/admin/dashboard/subCategories">
+                <div style={{ transform: 'rotate(180deg)' }}>
+                  {' '}
+                  <MdOutlineCategory />
+                </div>
+
+                <span className={styles.show}>Sub-Categories</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.sidebar__dropdown}>
+          <div className={styles.sidebar__dropdown_heading}>
+            <div className={styles.show}>Categories / Subs</div>
+          </div>
+          <ul className={styles.sidebar__list}>
+            <li className={route == 'coupons' ? styles.active : ''}>
+              <Link href="/admin/dashboard/coupons">
+                <RiCoupon3Fill />
+                <span className={styles.show}>Coupons</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
