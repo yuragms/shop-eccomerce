c75b5e50df23fd1341a3d3bd4a76bcf8113f638c
yuragms <yura.gms@gmail.com>
2024-04-15 21:19:12 +0300

    134-toastify-setup-and-explained

--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -1,10 +1,21 @@
 import Layout from '@/components/admin/layout';
 import styles from '../../../styles/dashboard.module.scss';
+import { toast } from 'react-toastify';
 
 export default function dashboard() {
   return (
     <div>
-      <Layout>gfhfgfgsh</Layout>
+      <Layout>
+        {/* <button
+          onClick={() =>
+            toast.error('Everything is working fine !!', {
+              position: 'bottom-right',
+            })
+          }
+        >
+          Click to show toastify
+        </button> */}
+      </Layout>
     </div>
   );
 }
