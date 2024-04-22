19ffb12ca2d9b549ff5305b59db08f7ac33bcbb3
yuragms <yura.gms@gmail.com>
2024-04-22 11:46:35 +0300

    136-create-category-start

--- components/admin/categories/Create.js
+++ components/admin/categories/Create.js
@@ -1,5 +1,44 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
+import * as Yup from 'yup';
+import { Form, Formik } from 'formik';
+import AdminInput from '@/components/inputs/admininput';
 
 export default function Create() {
-  return <div>Create</div>;
+  const [name, setName] = useState('');
+  const validate = Yup.object({
+    name: Yup.string(),
+  });
+  const submitHandler = async () => {};
+  return (
+    <>
+      <Formik
+        enableReinitialize
+        initialValues={{ name }}
+        validationSchema={validate}
+        onSubmit={() => {
+          submitHandler();
+        }}
+      >
+        {(formik) => (
+          <Form>
+            <div className={styles.header}>Create a Category</div>
+            <AdminInput
+              type="text"
+              Label="Name"
+              name="name"
+              placholder="Category name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button
+              type="submit"
+              className={`${styles.btn} ${styles.btn__primary}`}
+            >
+              <span>Add Category</span>
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 }
