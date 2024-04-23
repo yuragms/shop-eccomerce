c311659c2b1063b1b4d073bc8ec1bba4256047e0
yuragms <yura.gms@gmail.com>
2024-04-23 13:46:35 +0300

    138-create-category-submit-and-api-fixing-regex-issue

--- components/admin/categories/Create.js
+++ components/admin/categories/Create.js
@@ -3,17 +3,31 @@ import styles from './styles.module.scss';
 import * as Yup from 'yup';
 import { Form, Formik } from 'formik';
 import AdminInput from '@/components/inputs/admininput';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
-export default function Create() {
+export default function Create({ setCategories }) {
   const [name, setName] = useState('');
   const validate = Yup.object({
     name: Yup.string()
       .required('Category name is required.')
       .min(2, 'Category name must be between 2 and 30 characters.')
       .max(30, 'Category name must be between 2 and 30 characters.')
-      .matches(/^[aA-zZ]/, 'Numbers and special charcters are not allowed.'),
+      .matches(
+        /^[a-zA-Z\s]*$/,
+        'Numbers and special charcters are not allowed.'
+      ),
   });
-  const submitHandler = async () => {};
+  const submitHandler = async () => {
+    try {
+      const { data } = await axios.post('/api/admin/category', { name });
+      setCategories(data.categories);
+      toast.success(data.message);
+      console.log(data.categories);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
   return (
     <>
       <Formik
---
+++ pages/api/admin/category.js
@@ -0,0 +1,31 @@
+import { createRouter } from 'next-connect';
+import db from '@/utils/db';
+import auth from '@/middware/auth';
+import slugify from 'slugify';
+import Category from '@/models/Category';
+
+const router = createRouter().use(auth);
+
+router.post(async (req, res) => {
+  try {
+    const { name } = req.body;
+    db.connectDb();
+    const test = await Category.findOne({ name });
+    if (test) {
+      return res
+        .status(400)
+        .json({ message: 'Category already exist, Try a different name' });
+    }
+    await new Category({ name, slug: slugify(name) }).save();
+    db.disconnectDb();
+    res.json({
+      message: `Category ${name} has been created successfully.`,
+      categories: await Category.find({}).sort({ updatedAt: -1 }),
+    });
+  } catch (error) {
+    db.disconnectDb();
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router.handler();
