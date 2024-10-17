d930274fc332af5dcd5c6aa2ce5c7afe99d84e85
yuragms <yura.gms@gmail.com>
2024-10-17 11:49:45 +0300

    195-browse-heading-tags-listing-products

--- pages/browse.js
+++ pages/browse.js
@@ -1,17 +1,48 @@
 import db from '@/utils/db';
-// import styles from './styles.module.scss';
+import styles from '../styles/browse.module.scss';
 import SubCategory from '@/models/SubCategory';
 import Category from '@/models/Category';
 import Product from '@/models/Product';
 import { filterArray, randomize, removeDuplicates } from '@/utils/arrays_utils';
+import Header from '@/components/header';
+import Link from 'next/link';
+import ProductCard from '@/components/productCard';
+// import ProductCard from '@/components/admin/products/productCard';
 
-export default function browse() {
-  return <div>browse</div>;
+export default function browse({ categories, products }) {
+  return (
+    <div className={styles.browse}>
+      <Header />
+      <div className={styles.browse__container}>
+        <div className={styles.browse__path}>Home / Browse</div>
+        <div className={styles.browse__tags}>
+          {categories.map((c) => (
+            <Link href="" key={c._id}>
+              {c.name}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.browse__store}>
+          <div
+            className={`${styles.browse__store_filters} ${styles.scrollbar}`}
+          >
+            <button className={styles.browse__clearBtn}>Clear All (3)</button>
+          </div>
+          <div className={styles.browse__store_products}>
+            {products.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(ctx) {
   await db.connectDb();
   let productsDb = await Product.find({}).sort({ createdAt: -1 }).lean();
+  let products = randomize(productsDb);
   let categories = await Category.find().lean();
   let subCategories = await SubCategory.find()
     .populate({
@@ -33,7 +64,8 @@ export async function getServerSideProps(ctx) {
   await db.disconnectDb();
   return {
     props: {
-      //   products: JSON.parse(JSON.stringify(products)),
+      categories: JSON.parse(JSON.stringify(categories)),
+      products: JSON.parse(JSON.stringify(products)),
     },
   };
 }
