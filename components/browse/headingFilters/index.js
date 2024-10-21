45aa28eb6c3182f5a5a5daabd70c657f7aa873c7
yuragms <yura.gms@gmail.com>
2024-10-21 09:25:51 +0300

    199-17-54

--- components/browse/headingFilters/index.js
+++ components/browse/headingFilters/index.js
@@ -1,13 +1,18 @@
 import { Tooltip } from '@mui/material';
 import styles from './styles.module.scss';
+import { AiTwotoneStar } from 'react-icons/ai';
+import { useState } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
+import Link from 'next/link';
 
 export default function HeadingFilters() {
+  const [show, setShow] = useState(true);
   return (
     <div className={styles.filters}>
       <div className={styles.filters__price}>
         <span>Price :</span>
         <input type="number" placeholder="min" min="0>" />
-        <input type="number" placeholder="min" min="0>" />
+        <input type="number" placeholder="max" min="0>" />
       </div>
       <div className="stylesfilters__priceBtns">
         <Tooltip
@@ -19,6 +24,87 @@ export default function HeadingFilters() {
             <span style={{ height: '10%' }}></span>
           </button>
         </Tooltip>
+        <Tooltip
+          title={<h2>Check out products between 10$ and 50$</h2>}
+          placement="top"
+          arrow
+        >
+          <button>
+            <span></span>
+          </button>
+        </Tooltip>
+        <Tooltip
+          title={<h2>Check out products between 50$ and 100$</h2>}
+          placement="top"
+          arrow
+        >
+          <button>
+            <span style={{ height: '50%' }}></span>
+          </button>
+        </Tooltip>
+        <Tooltip
+          title={<h2>Check out products between 100$ and 500$</h2>}
+          placement="top"
+          arrow
+        >
+          <button>
+            <span style={{ height: '75%' }}></span>
+          </button>
+        </Tooltip>
+        <Tooltip
+          title={<h2>Check out products for more than 500$</h2>}
+          placement="top"
+          arrow
+        >
+          <button>
+            <span style={{ height: '100%' }}></span>
+          </button>
+        </Tooltip>
+      </div>
+      <div className={styles.filters__shipping}>
+        <input type="checkbox" name="shipping" id="shipping" />
+        <label htmlFor="shipping">Free Shipping</label>
+      </div>
+      <div className={styles.filters__rating}>
+        <input type="checkbox" name="rating" id="rating" />
+        <label htmlFor="rating">
+          <AiTwotoneStar />
+          <AiTwotoneStar />
+          <AiTwotoneStar />
+          <AiTwotoneStar /> & up
+        </label>
+      </div>
+      <div className={styles.filters__sort}>
+        <span>Sort by</span>
+        <div className={styles.filters__sort_list}>
+          <button
+            onMouseOver={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+          >
+            Recomeend
+            <div
+              style={{ transform: `${show ? 'rotate(180deg)' : 'rotate(0'}` }}
+            >
+              <IoIosArrowDown />
+            </div>
+          </button>
+          <ul
+            style={{
+              transform: `${show ? 'scale3d(1,1,1)' : 'scale3d(1,0,1)'}`,
+            }}
+          >
+            <li>
+              <Link href="">
+                <b>Recommend</b>
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <b>Recommend</b>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
